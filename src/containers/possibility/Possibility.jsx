bdec0f963002b45ab8545f0f27c67d30685763d0
Kwonojae <rnjsdhwo1325@gmail.com>
2022-07-27 02:40:35 +0900

    responsive website complete

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './possibility.css';
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className='gpt3__possibility section__padding' id='possibility'>
+            <div className='gpt3__possibility-image'>
+                <img src={possibilityImage} alt="possibilityImage" />
+            </div>
+            <div className='gpt3__possibility-content'>
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore sint temporibus tenetur praesentium dolore eligendi facere quaerat veniam, recusandae excepturi eaque blanditiis qui sunt. Voluptatum nam quibusdam eum at dolorem!</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
         </div>
+
     );
 };
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            WhatGPT3
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className='gpt3__whatgpt3-feature'>
+                <Feature title="What is GPT-3" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim temporibus cumque aliquid maxime dolore incidunt perferendis ipsum hic? Nostrum, repudiandae? Repellendus doloribus eum culpa deserunt voluptates quia rerum necessitatibus? Itaque?"/>
+            </div>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>
+                    
+                    The possibilties are beyond your imagination
+                </h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className='gpt3__whatgpt3-container'>
+                <Feature title="Chatbots" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim temporibus cumque aliquid maxime dolore incidunt perferendis ipsum hic? Nostrum, repudiandae? Repellendus doloribus eum culpa deserunt voluptates quia rerum necessitatibus? Itaque?"/>
+                <Feature title="Knowledgebase" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim temporibus cumque aliquid maxime dolore incidunt perferendis ipsum hic? Nostrum, repudiandae? Repellendus doloribus eum culpa deserunt voluptates quia rerum necessitatibus? Itaque?"/>
+                <Feature title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim temporibus cumque aliquid maxime dolore incidunt perferendis ipsum hic? Nostrum, repudiandae? Repellendus doloribus eum culpa deserunt voluptates quia rerum necessitatibus? Itaque?"/>
+            </div>
         </div>
     );
 };
